ab304ac59631499bc500550bacd394787988ebe2
Deepak Singh Bohra <deepaksinghbohra242@gmail.com>
2024-02-05 20:58:02 +0530

    doen

---
+++ some/src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+
+    static class Test {
+        int data;
+        void run(){
+            System.out.println("something");
+        }
+    }
+
+    public static void main(String[] args) {
+            Test t1 = new Test();
+            t1.run();
+            char ch = 66;
+            System.out.println(ch);
+    }
+}
---
+++ some/src/Tut1.java
@@ -0,0 +1,81 @@
+import java.util.Scanner;
+
+public class Tut1 {
+    public static void main(String[] args) {
+//        int age =10 ;
+//        if(age <=10){
+//            System.out.println("child");
+//        }else if(age <19){
+//            System.out.println("teenager");
+//        }else{
+//            System.out.println("adult");
+//        }
+//        if(age <=10) {
+//            System.out.println("child");
+//        }
+//        if(age <19){
+//            System.out.println("teenager");
+//        }else{
+//            System.out.println("adult");
+//        }
+
+        // nested if
+//        if(true){
+//            if(true){
+//                if(true){
+//
+//                }else{
+//                }
+//            }else{
+//            }
+//        }else{
+//        }
+
+//        if(true)
+//            if (true)
+//                if(true)
+//                    if (true) System.out.println("");
+//                        else
+//                        System.out.println();
+//                            else
+//                    System.out.println();
+
+//        char ch = 'a';
+//        switch (ch){
+//            case 'a' :                  System.out.println("sdhf");
+//            case 'b' :                 System.out.println("123");
+//            case 'c' :                 System.out.println("456");
+//            default:                 System.out.println("455");
+//            break;
+//        }
+
+        //loops
+//        for and while are entry control loop
+//        but do while is a exit controll loop
+//        int i = 5;
+//        do {
+//            System.out.println("123");
+//        }while(i<5);
+//        while(i<5){
+//            System.out.println("123");
+//        }
+//        for(int i=0;i<5;i++){
+//            System.out.println(i);
+//        }
+//        1. initialization
+//        2. condition check
+//        3. body
+//        4.increment ya decrement
+
+        // homework
+//        create a calculator 1. addition 2. substraction 3. multipy 4. reminder 5 exit
+
+//        Scanner sc = new Scanner(System.in);
+//        int a = sc.nextInt();
+//        int b = sc.nextInt();
+//        System.out.println(a+b);
+//                first case add sub mul di
+//                dusra mujse do no . lega
+//                wo operation
+    }
+}
