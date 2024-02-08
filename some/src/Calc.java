d1d92e8312c3e233a624061f235603fb0a395007
Deepak Singh Bohra <deepaksinghbohra242@gmail.com>
2024-02-08 21:01:40 +0530

    done

---
+++ some/src/Calc.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+
+import static java.lang.System.exit;
+
+public class Calc {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int choose;
+        do {
+            System.out.println("Choose one option");
+            System.out.println("1.Addition \n2.Subtraction \n3.Multipication \n4.Reminder \n5.Exit");
+            int ch = sc.nextInt();
+            System.out.println("Select first number");
+            int a = sc.nextInt();
+            System.out.println("Select second number");
+            int b = sc.nextInt();
+            int res = 0;
+            switch (ch) {
+                case 1:
+                    res = a + b;
+                    break;
+                case 2:
+                    res = a - b;
+                    break;
+                case 3:
+                    res = a * b;
+                    break;
+                case 4:
+                    res = a % b;
+                    break;
+                default:
+                    exit(0);
+            }
+            System.out.println("The result is " + res);
+            System.out.println("if you want to continue select 1");
+            choose = sc.nextInt();
+        } while (choose == 1);
+    }
+}
---
+++ some/src/Tut2.java
@@ -0,0 +1,53 @@
+public class Tut2 {
+    public static void main(String[] args) {
+        // ternary operator
+//        int age =10;
+//        boolean out ;
+//        if(age>10){
+//            out = true;
+//        }
+//        else{
+//            out = false;
+//        }
+//
+//        out = age > 10 ? true : false;
+        // break
+//        for(int i=0;i<3;i++){
+//            System.out.print(i);   //012
+//        }
+
+        for(int i=0;i<5;i++){
+            for(int j=0;j<5;j++){
+                if(i==3){
+                    break;
+                }
+                System.out.print(i + j);
+            }
+            for(int j=0;j<5;j++){
+                if(i>=3){
+                    System.out.println(j + i);
+                }else{
+                    break;
+                }
+            }
+        }
+
+        for (int i=0;i<5;i++){
+            for(int j=0;j<5;j++){
+                if(i==3 || j==4){
+                    break;
+                }
+                System.out.print(i + j);
+            }
+            System.out.println();
+            for (int j=5 ;j>0 ;j++){
+                if(i==3 || j==4){
+                    break;
+                }
+                System.out.print(i + j);
+            }
+        }
+
+
+    }
+}
