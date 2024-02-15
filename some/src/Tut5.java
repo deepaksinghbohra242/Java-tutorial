45e53f0f87a5d246be14c7af9fe6c2ed936f35c0
Deepak Singh Bohra <deepaksinghbohra242@gmail.com>
2024-02-15 21:02:45 +0530

    sione

---
+++ some/src/Tut5.java
@@ -0,0 +1,62 @@
+import javax.lang.model.element.NestingKind;
+
+public class Tut5 {
+
+
+    public static void main(String[] args) {
+//        int [][][] a = {{{1,2,3},{4,5,6},{7,8,9}},
+//                        {{1,2,3},{4,5,6},{7,8,9}},
+//                        {{1,2,3},{4,5,6},{7,8,9}}};
+//        int n = a.length;
+//        int m = a[0].length;
+//        int o = a[0][0].length;
+//        for (int i=0;i<n;i++){
+//            for(int j=0;j<m;j++){
+//                for(int k =0;k<o;k++){
+//                    System.out.print(a[i][j][k]+ " ");
+//                }
+//                System.out.println();
+//            }
+//            System.out.println();
+//        }
+
+//        int a [] = {1,23,4,56,};
+//        String s[] = {"adfj", "fsadf","asf"};
+//        String s1 = "sadfaer";
+//        char ch [] = {'a','d','i','i'};
+//        String s2 = "Adii";
+//        int m = a.length;
+//        int n = s.length;
+//        int o = s2.length();
+
+//        char ch1 = 'a';
+//        char ch2 = "a /0";
+//        string s2 = "astdfkdrhjs";
+//        String s3 = "adii";
+//        int n = s3.length();
+//        System.out.println(n);
+
+//        Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+//
+//                You may assume that each input would have exactly one solution, and you may not use the same element twice.
+//
+//        You can return the answer in any order.
+
+
+        int [] arr = {3,5,2,3,7,11,15};
+        int target = 9;
+        int [] ans;  // 2,5
+
+        int [][][] ar = {{{1,2,3,2,32,2}},{{1,2,3}},{{1,3,4,5,6}},{{1}}};
+
+
+    }
+}
+
+//class String{
+//    char [] s1;
+//    int length(s1){
+//        for(int i=0;s[i] != '\0';i++);
+//        return 2
+//    }
+//}
